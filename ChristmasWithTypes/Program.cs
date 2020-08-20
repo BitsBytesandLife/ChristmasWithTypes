4793866130b836a7eb65ff9e76b5e833cb1260ba
BK <brian_j_king@me.com>
2020-08-20 15:10:43 -0500

    added5GiftsforMyHolidayWishGiftBugBJKAug2020

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,7 +14,7 @@ namespace ChristmasWithTypes
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]; 
+            xmas.Presents =  new string[4] {"XBox","Nintedo Switch","Iphone X","Playstation 5" }; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
